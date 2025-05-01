a64735044c1d8d36293fa16a30403a161c4b019a
Gabriel-Hiss <bielpaes912@gmail.com>
2025-05-01 01:46:25 -0300

    fix build eror

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -18,6 +18,8 @@ export async function login(email: string, password: string) {
         };
 
 
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
         const response = await fetch(`${baseURL}/users/management/login`, requestOptions)
         if (!response.ok) {
             alert('Login failed');

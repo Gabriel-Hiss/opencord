fcd580d6bc20140e1c6e894ac6c7ea1e8b11a8d2
Gabriel-Hiss <bielpaes912@gmail.com>
2025-05-01 01:33:02 -0300

    add cors mapping

--- src/main/java/net/opencord/config/WebMvcConfig.java
+++ src/main/java/net/opencord/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package net.opencord.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
